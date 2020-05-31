2849376e7637de0a14f0de52e0bd66ffa8f39189
maurosbicego <mauro@sbicego.ch>
2020-05-31 17:12:27 +0200

    -

--- pages/account/index.vue
+++ pages/account/index.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="container mx-auto bg-white rounded-lg p-3 mt-3">
   <h1 class="text-2xl font-bold mb-2">Personal Dashboard</h1>
-  <p class="text-lg">Your challange </p>
+  <p class="text-lg">Your {{ challenges }}</p>
 </div>
 </template>
 
@@ -13,7 +13,7 @@ export default {
     }
   },
   mounted() {
-    this.$axios.$get(process.env.API_URL + '/api/v1/challenge').then(res => {
+    this.$axios.$post(process.env.API_URL + '/api/v1/challenge/user').then(res => {
       this.challenges = res.message
     }).catch(error => {
       console.log(error)

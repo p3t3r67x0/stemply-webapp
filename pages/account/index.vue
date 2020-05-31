851e212ad297fcd2d94d65e461d273561eb5a037
maurosbicego <mauro@sbicego.ch>
2020-05-31 18:29:25 +0200

    Added possibility to switch to showing all challenges

--- pages/account/index.vue
+++ pages/account/index.vue
@@ -3,19 +3,12 @@
   <h1 class="text-2xl font-bold mb-2">Personal Dashboard</h1>
   <hr style="width: 365px">
   <div>
-    <h2 v-if="challenges.length != 0" class="text-xl font-bold mb-2 text-center my-4">Hello</h2>
+    <h2 v-if="challenges.length != 0" class="text-xl font-bold mb-2 text-center my-4">Your current challenge:</h2>
     <hr style="width: 100px" class="mx-auto my-4">
   </div>
-  <div class="mb-2">
-    <div class="form-switch inline-block align-middle">
-        <input type="checkbox" name="1" id="1" class="form-switch-checkbox" />
-        <label class="form-switch-label" for="1"></label>
-    </div>
-    <label class="text-xs text-grey-dark" for="1">Default</label>
-</div>
   <ul class="flex justify-center">
     <div v-for="challenge in challenges">
-      <div class="max-w-sm rounded overflow-hidden shadow-lg" v-if="challenges[showing]._id === challenge._id">
+      <div class="max-w-sm rounded overflow-hidden shadow-lg" v-if="challenges[showing]._id === challenge._id || showall">
         <div class="px-6 py-4">
           <div class="font-bold text-xl mb-2">{{ challenge.title }}</div>
           <p class="text-gray-700 text-base">
@@ -23,13 +16,19 @@
           </p>
         </div>
         <hr>
-        <div class="px-6 py-4 flex">
+        <div class="px-6 py-4 flex" v-if="!showall">
           <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2 mr-auto" :class="{ 'opacity-50 cursor-not-allowed': showing === 0}" :disabled="showing === 0" v-on:click="showing-=1">Previous</button>
           <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2 ml-auto" :class="{ 'opacity-50 cursor-not-allowed': showing === challenges.length-1}" :disabled="showing === challenges.length-1" v-on:click="showing+=1">Next</button>
         </div>
       </div>
     </div>
   </ul>
+  <div class="mb-2">
+    <div class="form-switch inline-block align-right ml-auto">
+        <input type="checkbox" name="showallswitch" v-model="showall" id="showallswitch" class="form-switch-checkbox" />
+        <label class="form-switch-label" for="showallswitch">Show all challenges</label>
+    </div>
+</div>
 </div>
 </template>
 
@@ -38,7 +37,8 @@ export default {
   data() {
     return {
       challenges: [],
-      showing: 0
+      showing: 0,
+      showall: false
     }
   },
   mounted() {

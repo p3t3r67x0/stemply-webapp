e6b449ca61e002244791d374abb7a86f7d517ff2
Aurelius Wendelken <webtobesocial@gmail.com>
2020-06-01 01:48:35 +0200

    fixed bug when user has no challenges assigned

--- pages/admin/subscribtion/index.vue
+++ pages/admin/subscribtion/index.vue
@@ -1,21 +1,18 @@
 <template>
 <ul class="container mx-auto mt-3">
+  <h1 class="text-2xl font-bold mb-6">Subscription</h1>
   <li v-for="challenge in challenges" class="bg-white rounded-lg p-3 mb-6">
-    <div class="flex justify-betweeen mb-1">
-      <h3 class="w-full md:w-8/12 text-xl font-bold">{{ challenge.title }}</h3>
-    </div>
-    <span class="inline-block bg-gray-200 text-gray-700 text-sm font-semibold rounded-lg px-3 py-1 mb-3">{{ secondsToDays(challenge.duration) }}</span>
-    <p class="text-lg mb-6">{{ challenge.content }}</p>
+    <h3 class="w-full md:w-8/12 text-xl font-bold mb-2">{{ challenge.title }}</h3>
     <h4 class="w-full md:w-8/12 text-xl mb-1">Manage user subscription</h4>
     <ul>
-      <li v-for="user in users" class="odd:bg-gray-200 p-2">
+      <li v-for="user in users" class="odd:bg-gray-200 even:bg-gray-100 p-2">
         <div class="flex justify-between">
           <div>
             {{ user.email }}
           </div>
           <div class="text-right">
-            <button type="button" @click="toggleSubscribe(challenge._id, user._id)" :class="[matchChallenge(challenge._id, user.challenges) ? 'bg-blue-500 hover:bg-blue-600' : 'bg-green-500 hover:bg-green-600']"
-              class="focus:outline-none rounded text-white text-sm font-medium tracking-wide px-2 py-1">{{ matchChallenge(challenge._id, user.challenges) ? 'Unsubscribe' : 'Subscribe'}}</button>
+            <button type="button" @click="toggleSubscribe(challenge._id, user._id)" :class="[matchChallenge(challenge._id, user) ? 'bg-blue-500 hover:bg-blue-600' : 'bg-green-500 hover:bg-green-600']"
+              class="focus:outline-none rounded text-white text-sm font-medium tracking-wide px-2 py-1">{{ matchChallenge(challenge._id, user) ? 'Unsubscribe' : 'Subscribe'}}</button>
           </div>
         </div>
       </li>
@@ -34,7 +31,6 @@ export default {
   },
   mounted() {
     this.$axios.$get(process.env.API_URL + '/api/v1/challenge').then(res => {
-      console.log(res)
       this.challenges = res.message
     }).catch(error => {
       console.log(error.response.data)
@@ -66,8 +62,12 @@ export default {
         console.log(error.response.data)
       })
     },
-    matchChallenge(challengeId, challenges) {
-      return challenges.includes(challengeId)
+    matchChallenge(challengeId, user) {
+      if (!user.hasOwnProperty('challenges')) {
+        return false
+      }
+
+      return user.challenges.includes(challengeId)
     },
     secondsToDays(seconds) {
       const days = Math.round(seconds / 3600 / 24)

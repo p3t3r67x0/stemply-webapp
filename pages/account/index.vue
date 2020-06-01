497a40d19a18ad3cd56b6a2fd6ca83531640a594
maurosbicego <mauro@sbicego.ch>
2020-06-01 11:13:29 +0200

    Dashboard-Cards are now responsive, Cards now show only excerpt of text

--- pages/account/index.vue
+++ pages/account/index.vue
@@ -1,33 +1,39 @@
 <template>
-<div class="container mx-auto bg-white rounded-lg p-3 mt-3">
+<div class="container mx-auto bg-white rounded-lg p-12 mt-3">
   <h1 class="text-2xl font-bold mb-2">Personal Dashboard</h1>
   <hr style="width: 365px">
   <div>
-    <h2 v-if="challenges.length != 0" class="text-xl font-bold mb-2 text-center my-4">Your current challenge:</h2>
+    <h2 v-if="challenges.length == 1" class="text-xl font-bold mb-2 text-center my-4">Your current challenge:</h2>
+    <h2 v-if="challenges.length > 1" class="text-xl font-bold mb-2 text-center my-4">Your current challenges:</h2>
     <hr style="width: 100px" class="mx-auto my-4">
   </div>
-  <ul class="flex justify-center">
-    <div v-for="challenge in challenges">
-      <div class="max-w-sm rounded overflow-hidden shadow-lg m-2" v-if="challenges[showing]._id === challenge._id || showall">
-        <img class="w-full" src="https://images.unsplash.com/photo-1590589195374-163308c534ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" alt="Forest">
-        <div class="px-6 py-4">
-          <div class="font-bold text-xl mb-2">{{ challenge.title }}</div>
-          <p class="text-gray-700 text-base">
-            {{ challenge.content }}
-          </p>
-        </div>
-        <hr>
-        <div class="px-6 py-4">
-          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">Duration: {{ challenge.duration / 3600 / 24}} days</span>
-        </div>
-        <div class="px-6 py-4 flex" v-if="!showall">
-          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2 mr-auto" :class="{ 'opacity-50 cursor-not-allowed': showing === 0}" :disabled="showing === 0" v-on:click="showing-=1">Previous</button>
-          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2 ml-auto" :class="{ 'opacity-50 cursor-not-allowed': showing === challenges.length-1}" :disabled="showing === challenges.length-1"
-            v-on:click="showing+=1">Next</button>
+  <div class="container my-12 mx-auto px-4 md:px-12">
+    <div class="flex flex-wrap -mx-1 lg:-mx-4" :class="{ 'flex justify-center' : !showall }">
+
+      <div class="my-1 px-1 w-full md:w-1/2 lg:my-2 lg:px-2 lg:w-1/3" v-for="challenge in challenges" v-if="challenges[showing]._id === challenge._id || showall">
+        <div class="max-w-sm rounded overflow-hidden shadow-lg m-2 container">
+          <img class="w-full" src="https://images.unsplash.com/photo-1590589195374-163308c534ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" alt="Forest">
+          <div class="px-6 py-4">
+            <div class="font-bold text-xl mb-2">{{ challenge.title }}</div>
+            <p class="text-gray-700 text-base">
+              <span v-if="challenge.content.length > excerptlength">{{ challenge.content.substring(0, excerptlength) }}...</span>
+              <span v-else>{{ challenge.content }}</span>
+            </p>
+          </div>
+          <hr>
+          <div class="px-6 py-4">
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">Duration: {{ challenge.duration / 3600 / 24}} days</span>
+          </div>
+          <div class="px-6 py-4 flex" v-if="!showall">
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2 mr-auto" :class="{ 'opacity-50 cursor-not-allowed': showing === 0}" :disabled="showing === 0" v-on:click="showing-=1">Previous</button>
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2 ml-auto" :class="{ 'opacity-50 cursor-not-allowed': showing === challenges.length-1}" :disabled="showing === challenges.length-1"
+              v-on:click="showing+=1">Next</button>
+          </div>
         </div>
       </div>
+
     </div>
-  </ul>
+  </div>
   <div class="mb-2">
     <div class="form-switch inline-block align-right ml-auto">
       <input type="checkbox" name="showallswitch" v-model="showall" id="showallswitch" class="form-switch-checkbox" />
@@ -43,7 +49,8 @@ export default {
     return {
       challenges: [],
       showing: 0,
-      showall: false
+      showall: false,
+      excerptlength: 200
     }
   },
   mounted() {

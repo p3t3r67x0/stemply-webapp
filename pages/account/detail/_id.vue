3d2751aa239691d863060771343be2c06642ce21
Aurelius Wendelken <webtobesocial@gmail.com>
2020-06-01 17:00:53 +0200

    fixed detail page content was not shown now it will be displayed

--- pages/account/detail/_id.vue
+++ pages/account/detail/_id.vue
@@ -5,7 +5,6 @@
       <h1 class="text-2xl font-bold mb-2 justify-content-left">{{ challenge.title }}</h1>
       <a href="/account" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-auto justify-content-right">All challenges</a>
     </div>
-    <hr style="width: 365px">
   </div>
   <div class="mx-8 mb-8">
     <p>{{ challenge.content }}</p>
@@ -25,10 +24,7 @@ export default {
       this.$axios.$post(process.env.API_URL + '/api/v1/challenge/detail', {
         'id': this.challengeId.trim()
       }).then(res => {
-        if (res.message[0]) {
-          console.log(res.message[0])
-          this.challenge = res.message[0]
-        }
+        this.challenge = res.message
       }).catch(error => {
         console.log(error)
       })

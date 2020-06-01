f470ff5f1366598c541da2a91331803b0ffb8093
Aurelius Wendelken <webtobesocial@gmail.com>
2020-06-01 16:11:37 +0200

    changed wording now it is called edit task

--- pages/admin/task/edit/_id.vue
+++ pages/admin/task/edit/_id.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="container bg-white rounded-lg mx-auto p-3">
-  <h1 class="text-2xl lg:text-4xl font-medium mb-2">Add task</h1>
+  <h1 class="text-2xl lg:text-4xl font-medium mb-2">Edit task</h1>
   <editor-component target="task" action="update" :taskId="taskId" :challengeId="challengeId" />
 </div>
 </template>

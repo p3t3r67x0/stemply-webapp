33b9f2ea404ab3575e8881c00a9a0823716b1656
Aurelius Wendelken <webtobesocial@gmail.com>
2020-06-03 21:07:22 +0200

    implemented datepicker in challenge editor and removed unused duration

--- pages/admin/challenge/_id.vue
+++ pages/admin/challenge/_id.vue
@@ -12,19 +12,31 @@
           type="text" placeholder="Learn to use the new media">
         <p v-if="errors.title" class="text-red-500 text-xs italic">Please fill out this field.</p>
       </div>
-      <div class="w-full mb-6">
-        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="duration">
-          Duration
-        </label>
-        <input name="duration" v-model="challenge.duration" v-bind:class="{'border-red-500': errors.duration}"
-          class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded p-3 mb-1 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="duration" type="duration" placeholder="Valid for three weeks">
-        <p v-if="errors.duration" class="text-red-500 text-xs italic">Please fill out this field.</p>
+      <div class="sm:flex w-full mb-6">
+        <div class="sm:w-1/2 md:w-1/3 lg:w-1/4 mb-6 md:mb-0 sm:mr-4">
+          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="fromDate">
+            From
+          </label>
+          <client-only>
+            <date-picker v-model="challenge.fromDate" format="DD-MM-YYYY" valueType="DD-MM-YYYY" :disabled-date="notBeforeToday" class="w-full focus:outline-none" placeholder="DD-MM-YYYY"></date-picker>
+          </client-only>
+          <p v-if="errors.fromDate" class="text-red-500 text-xs italic">Please fill out this field.</p>
+        </div>
+        <div class="sm:w-1/2 md:w-1/3 lg:w-1/4">
+          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="toDate">
+            To
+          </label>
+          <client-only>
+            <date-picker v-model="challenge.toDate" format="DD-MM-YYYY" valueType="DD-MM-YYYY" :disabled-date="notBeforeToday" class="w-full focus:outline-none" placeholder="DD-MM-YYYY"></date-picker>
+          </client-only>
+          <p v-if="errors.toDate" class="text-red-500 text-xs italic">Please fill out this field.</p>
+        </div>
       </div>
       <div class="w-full mb-6">
-        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="duration">
+        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="fromDate">
           Content
         </label>
-        <textarea name="duration" v-model="challenge.content" v-bind:class="{'border-red-500': errors.content}"
+        <textarea name="fromDate" v-model="challenge.content" v-bind:class="{'border-red-500': errors.content}"
           class="h-64 appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded p-3 mb-1 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="content" type="content"
           placeholder="Describe your chalenge here"></textarea>
         <p v-if="errors.content" class="text-red-500 text-xs italic">Please fill out this field.</p>
@@ -50,19 +62,28 @@ export default {
       showResponse: false,
       challenge: {
         title: '',
-        duration: '',
-        content: ''
+        content: '',
+        fromDate: '',
+        toDate: '',
       },
       errors: {
         title: false,
-        duration: false,
-        content: false
+        content: false,
+        fromDate: false,
+        toDate: false,
       }
     }
   },
   computed: {
     challengeId() {
       return this.$route.params.id
+    },
+    yesterday() {
+      const today = new Date()
+      const yesterday = new Date(today)
+      yesterday.setDate(yesterday.getDate() - 1)
+
+      return yesterday
     }
   },
   created() {
@@ -70,7 +91,11 @@ export default {
       this.$axios.$post(process.env.API_URL + '/api/v1/challenge/detail', {
         'id': this.challengeId.trim()
       }).then(res => {
-        this.challenge = res.message
+        this.challenge['_id'] = res.message._id
+        this.challenge['toDate'] = res.message.to
+        this.challenge['fromDate'] = res.message.from
+        this.challenge['content'] = res.message.content
+        this.challenge['title'] = res.message.title
       }).catch(error => {
         console.log(error.response.data)
       })
@@ -95,16 +120,26 @@ export default {
         }
       }
     },
-    'challenge.duration': function() {
-      if (this.challenge.duration > 0) {
-        this.errors.duration = false
+    'challenge.fromDate': function() {
+      if (this.challenge.fromDate) {
+        this.errors.fromDate = false
       } else {
-        this.errors.duration = true
+        this.errors.fromDate = true
+      }
+    },
+    'challenge.toDate': function() {
+      if (this.challenge.toDate) {
+        this.errors.toDate = false
+      } else {
+        this.errors.toDate = true
       }
     }
   },
   middleware: 'auth',
   methods: {
+    notBeforeToday(date) {
+      return date < this.yesterday
+    },
     submitForm: function(e) {
       const isValidForm = (currentValue) => currentValue !== true
 
@@ -116,17 +151,22 @@ export default {
         this.errors.title = true
       }
 
-      if (!this.challenge.duration) {
-        this.errors.duration = true
+      if (!this.challenge.fromDate) {
+        this.errors.fromDate = true
+      }
+
+      if (!this.challenge.toDate) {
+        this.errors.toDate = true
       }
 
       if (Object.values(this.errors).every(isValidForm) === true) {
         if (this.challengeId) {
           this.$axios.$put(process.env.API_URL + '/api/v1/challenge/detail', {
-            'id': this.challengeId.trim(),
-            'title': this.challenge.title.trim(),
-            'content': this.challenge.content.trim(),
-            'duration': this.challenge.duration
+            id: this.challengeId.trim(),
+            title: this.challenge.title.trim(),
+            content: this.challenge.content.trim(),
+            from_date: this.challenge.fromDate.trim(),
+            to_date: this.challenge.toDate.trim()
           }).then(res => {
             this.response = res.message
             this.showResponse = true
@@ -139,9 +179,10 @@ export default {
           })
         } else {
           this.$axios.post(process.env.API_URL + '/api/v1/challenge', {
-            'title': this.challenge.title.trim(),
-            'duration': this.challenge.duration.trim(),
-            'content': this.challenge.content.trim()
+            title: this.challenge.title.trim(),
+            content: this.challenge.content.trim(),
+            from_date: this.challenge.fromDate.trim(),
+            to_date: this.challenge.toDate.trim()
           }).then(res => {
             this.response = res.data.message
             this.showResponse = true

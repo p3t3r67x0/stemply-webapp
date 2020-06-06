bdc881436be721955baa3c29a1cd15e5df0b8df8
maurosbicego <mauro@sbicego.ch>
2020-06-06 20:03:41 +0200

    Challenge-Modal now scrollable and responsive

--- components/challengeModal.vue
+++ components/challengeModal.vue
@@ -1,34 +1,28 @@
 <template>
-  <div class="modal modal-active fixed w-full h-full top-0 left-0 flex items-center justify-center">
-    <div class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50" @click="closeModal"></div>
+<div class="modal modal-active fixed w-full h-screen top-0 left-0 flex items-center justify-center">
+  <div class="modal-overlay absolute w-full h-screen bg-gray-900 opacity-50" @click="closeModal"></div>
 
-    <div class="modal-container bg-white mx-16 rounded shadow-lg z-50">
-
-      <div class="modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50" v-on:click="closeModal">
-        <fa :icon="['fas', 'times']" v-on:click="closeModal" class="inline-block cursor-pointer text-xl lg:text-2xl w-5 mr-3" />
-      </div>
-
-      <!-- Add margin if you want to see some of the overlay behind the modal-->
-      <div class="modal-content py-4 text-left px-6">
-        <!--Title-->
-        <div class="flex justify-between items-center pb-3">
-          <p class="text-2xl font-bold" v-if="challenge">{{ challenge.title }}</p>
-          <div class="modal-close cursor-pointer z-50" v-on:click="closeModal">
-            <svg class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
-              <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
-            </svg>
-          </div>
+  <div class="modal-container bg-white mx-16 rounded shadow-lg z-50">
+    <div class="modal-content py-4 text-left px-6" style="height: 95vh">
+      <div class="flex justify-between items-center pb-3">
+        <p class="text-2xl font-bold" v-if="challenge">{{ challenge.title }}</p>
+        <div class="modal-close cursor-pointer z-50" v-on:click="closeModal">
+          <svg class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
+            <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+          </svg>
         </div>
-        <div class="flex" v-if="challenge">
-          <div v-if="challenge" class="w-auto border-r-2 p-4 m-4">
-            <vue-markdown-plus class="markdown" :source="challenge.content" />
+      </div>
+      <div class="xl:flex" v-if="challenge">
+        <div v-if="challenge" class="w-auto border-r-2 p-4 m-4 overflow-scroll modal-text">
+          <vue-markdown-plus class="markdown" :source="challenge.content" />
+        </div>
+        <div v-if="challenge" class="w-full p-4 m-4 w-2/3 p-4">
+          <div class="pb-3">
+            <h3 class="text-xl">Tasks</h3>
+            <hr>
           </div>
-          <div v-if="challenge" class="w-full p-4 m-4 w-2/3 p-4">
-            <div class="pb-3">
-              <h3 class="text-xl">Tasks</h3>
-              <hr>
-            </div>
-            <ul :key="challenge.tasks">
+          <div class="overflow-scroll modal-tasks">
+            <ul :key="challenge.tasks.length">
               <li v-for="task in challenge.tasks" :key="task._id" class="lg:flex justify-between w-full odd:bg-gray-100 even:bg-gray-200 px-2 py-3">
                 <div class="flex justify-between mr-3 mb-3 lg:mb-0">
                   <span :key="progressChanged">
@@ -50,21 +44,43 @@
             </ul>
           </div>
         </div>
-        <div class="flex justify-end pt-2">
-          <button class="modal-close px-4 bg-indigo-500 p-3 rounded-lg text-white hover:bg-indigo-400" v-on:click="closeModal">Close</button>
-        </div>
-
       </div>
+      <div class="flex justify-end pt-2">
+        <button class="modal-close px-4 bg-indigo-500 p-3 rounded-lg text-white hover:bg-indigo-400" v-on:click="closeModal">Close</button>
+      </div>
+
     </div>
   </div>
+</div>
 </template>
+<style scoped>
+.modal-text {
+  height: 40vh;
+}
+
+.modal-tasks {
+  height: 20vh;
+}
+
+
+@media (min-width: 1280px) {
+  .modal-text {
+    height: 75vh !important;
+  }
+
+  .modal-tasks {
+    height: 35vh !important;
+  }
+}
+</style>
 <script>
 import VueMarkdownPlus from 'vue-markdown-plus'
 
 export default {
   data() {
     return {
-      statusClass: ""
+      statusClass: "",
+      progressChanged: 0
     }
   },
   props: ['challenge', 'status'],
@@ -79,7 +95,27 @@ export default {
   methods: {
     closeModal() {
       this.$emit('clicked', false)
-    }
+    },
+    toggleProgressStatus(challengeId, taskId) {
+      this.$axios.$put(process.env.API_URL + '/api/v1/challenge/task/progress', {
+        challenge_id: challengeId,
+        task_id: taskId
+      }).then(res => {
+        console.log(this.$t(res.message))
+
+        const taskIndex = this.challenge.tasks.findIndex(task => task._id === taskId)
+
+        if (taskIndex >= 0) {
+          let task = this.challenge.tasks[taskIndex]
+          task['progress'] = res.progress
+
+          this.$set(this.challenge.tasks, taskIndex, task)
+          this.progressChanged += 1
+        }
+      }).catch(error => {
+        console.log(error)
+      })
+    },
   },
   components: {
     VueMarkdownPlus

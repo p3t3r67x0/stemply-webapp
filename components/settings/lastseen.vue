0188c6f3515150f356bef2fc3541ab66f2aaa573
Aurelius Wendelken <webtobesocial@gmail.com>
2020-06-07 00:08:39 +0200

    modified component lastseen to display last successful logins

--- components/settings/lastseen.vue
+++ components/settings/lastseen.vue
@@ -1,17 +1,21 @@
 <template>
 <div>
   <h2 class="text-2xl mb-4">Last Logins</h2>
-  <table class="w-full text-md text-white bg-blue-900 rounded">
-    <tr class="border-b">
-      <th class="text-left p-3">Login</th>
-      <th class="text-left p-3">IP</th>
-      <th class="text-left p-3">Browser</th>
-    </tr>
-    <tr v-for="(login, i) in logins" :key="login.id" class="border-b hover:bg-blue-200">
-      <td data-label="Login" class="p-3 text-gray-700">{{ login.last_seen }}</td>
-      <td data-label="IP" class="p-3 text-gray-700">{{ login.ip_address }}</td>
-      <td data-label="Browser" class="p-3 text-gray-700">{{ login.browser }}</td>
-    </tr>
+  <table class="w-full text-md text-gray-800">
+    <thead>
+      <tr class="border-b">
+        <th class="text-left p-3">Login</th>
+        <th class="text-left p-3">Address</th>
+        <th class="text-left p-3">Browser</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(login, i) in logins" :key="login._id" class="border-b hover:bg-gray-200">
+        <td data-label="Login" class="p-3 text-gray-700">{{ login.datetime }}</td>
+        <td data-label="Address" class="p-3 text-gray-700">{{ login.address }}</td>
+        <td data-label="Browser" class="p-3 text-gray-700">{{ login.browser }}</td>
+      </tr>
+    </tbody>
   </table>
 </div>
 </template>
@@ -24,12 +28,17 @@ export default {
     }
   },
   computed: {
-    loadinIndicator() {
-      return this.$store.state.loading
+    userId() {
+      return this.$store.state.userId
     },
   },
   components: {},
-  created() {},
+  created() {
+    this.$axios.$get(process.env.API_URL + '/api/v1/user/lastseen/' + this.userId).then(res => {
+      console.log(res)
+      this.logins = res.message
+    })
+  },
   methods: {}
 }
 </script>

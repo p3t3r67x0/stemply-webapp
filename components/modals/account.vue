4509a898b6824d79b8676a9e1cc242fb4b6f40e9
Aurelius Wendelken <webtobesocial@gmail.com>
2020-06-10 23:28:33 +0200

    added modals directory and implemented confirm delete account modal

---
+++ components/modals/account.vue
@@ -0,0 +1,67 @@
+<template>
+<div v-if="showModal" class="fixed bottom-0 inset-x-0 px-3 pb-3 sm:inset-0 sm:flex sm:items-center sm:justify-center">
+  <div class="fixed inset-0 transition-opacity">
+    <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+  </div>
+  <div v-click-outside="closeModal" class="bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+    <div class="bg-white p-3">
+      <div class="sm:flex sm:items-start">
+        <div class="">
+          <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-headline">
+            Delete account
+          </h3>
+          <div class="mt-2">
+            <p class="text-sm lg:text-md leading-5 text-gray-800">
+              Are you sure you want to delete your account? All of your data will be permanently removed. This action cannot be undone.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="px-3 py-3 sm:px-3 flex justify-end">
+      <span class="flex w-full rounded-md sm:mt-0 sm:w-auto mr-3 sm:mr-0">
+        <button @click="closeModal" type="button"
+          class="inline-flex justify-center w-full rounded border border-gray-300 px-3 py-2 bg-gray-200 hover:bg-gray-300 text-base leading-6 font-medium text-gray-700 focus:outline-none focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+          Cancel
+        </button>
+      </span>
+      <span class="flex w-full rounded-md sm:ml-3 sm:w-auto">
+        <button @click="confirmDelete" type="button"
+          class="inline-flex justify-center w-full rounded border border-transparent px-3 py-2 bg-red-500 text-base leading-6 font-medium text-white hover:bg-red-600 focus:outline-none focus:shadow-outline-red transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+          Delete account
+        </button>
+      </span>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  props: ['openModal'],
+  computed: {
+    showModal() {
+      return this.openModal
+    }
+  },
+  mounted() {
+    window.addEventListener('keydown', (e) => {
+      if (e.key === 'Escape') {
+        this.closeModal()
+      }
+    })
+  },
+  methods: {
+    closeModal() {
+      this.$emit('closeModal')
+    },
+    confirmDelete() {
+      this.$emit('confirmDelete')
+      this.closeModal()
+    }
+  }
+}
+</script>
--- components/settings/account.vue
+++ components/settings/account.vue
@@ -1,52 +1,65 @@
 <template>
 <div>
+  <modal-component @confirmDelete="deleteAccount" @closeModal="closeModal" :openModal="showModal" />
+
   <h2 class="text-2xl mb-4">Before You Delete</h2>
   <p class="lg:w-2/3 lg:text-xl mb-4">Please be aware that deleting your account is irreversible. We can’t help you recover your account after deletion. When you delete your account, you remove all of your challenges, tasks, and tags. Your username
     will be released for re-use.</p>
-  <button type="submit" @click="deleteAccount" class="bg-red-500 hover:bg-red-600 focus:outline-none rounded text-white text-sm font-medium tracking-wide px-3 py-2">Delete Account</button>
+  <button type="submit" @click="openConfirmModal" class="bg-red-500 hover:bg-red-600 focus:outline-none rounded text-white text-sm font-medium tracking-wide px-3 py-2">Delete Account</button>
 </div>
 </template>
 
 <script>
+import ModalComponent from '@/components/modals/account'
 const Cookie = require('js-cookie')
 
 export default {
   data() {
-    return {}
+    return {
+      showModal: false,
+      confirmDelete: false
+    }
+  },
+  components: {
+    ModalComponent
   },
   methods: {
+    closeModal() {
+      this.showModal = false
+    },
+    openConfirmModal() {
+      this.showModal = true
+    },
     deleteAccount() {
-      if (confirm('Are you sure to delete your account?')) {
-        this.$axios.$post(process.env.API_URL + '/api/v1/user/delete/account').then(res => {
-          console.log(res)
+      this.$axios.$post(process.env.API_URL + '/api/v1/user/delete/account').then(res => {
+        console.log(res)
 
-          this.$store.commit('updateUserId', null)
-          this.$store.commit('updateUserName', null)
-          this.$store.commit('updateUserRoles', null)
-          this.$store.commit('updateUserAvatar', null)
-          this.$store.commit('updateUserAvatarUrl', null)
-          this.$store.commit('updateRefreshToken', null)
-          this.$store.commit('updateAccessToken', null)
+        this.$store.commit('updateUserId', null)
+        this.$store.commit('updateUserName', null)
+        this.$store.commit('updateUserRoles', null)
+        this.$store.commit('updateUserAvatar', null)
+        this.$store.commit('updateUserAvatarUrl', null)
+        this.$store.commit('updateRefreshToken', null)
+        this.$store.commit('updateAccessToken', null)
 
-          Cookie.remove('USER_ID')
-          Cookie.remove('USER_ROLES')
-          Cookie.remove('USER_AVATAR_URL')
-          Cookie.remove('USER_ACCESS_TOKEN')
-          Cookie.remove('USER_REFRESH_TOKEN')
+        Cookie.remove('USER_ID')
+        Cookie.remove('USER_ROLES')
+        Cookie.remove('USER_AVATAR_URL')
+        Cookie.remove('USER_ACCESS_TOKEN')
+        Cookie.remove('USER_REFRESH_TOKEN')
 
-          this.$router.push(this.localePath({
-            name: 'signup'
-          }))
-        }).catch(error => {
-          if (error.hasOwnProperty('response')) {
-            this.response = error.response.data.message
-            this.responseError = true
-            this.showResponse = true
-          } else {
-            console.log(error)
-          }
-        })
-      }
+        this.$router.push(this.localePath({
+          name: 'signup'
+        }))
+      }).catch(error => {
+        if (error.hasOwnProperty('response')) {
+          this.response = error.response.data.message
+          this.responseError = true
+          this.showResponse = true
+        } else {
+          console.log(error)
+        }
+      })
     }
   }
 }

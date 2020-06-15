58576c8927932a45e1a95b579169bfa52fd4a7eb
Aurelius Wendelken <webtobesocial@gmail.com>
2020-06-15 16:08:15 +0200

    added posibility to append a form to a challenge task

--- components/editors/task.vue
+++ components/editors/task.vue
@@ -39,14 +39,47 @@
         placeholder="Describe your chalenge here"></textarea>
       <p v-if="errors.content" class="text-red-500 text-xs italic">Please fill out this field.</p>
     </div>
-    <p class="text-right">
+    <ul v-if="forms.length > 0" class="mb-6">
+      <li v-for="form in forms">
+        <h2 class="text-xl font-semibold mb-3">Question</h2>
+        <div class="bg-gray-200 rounded-lg p-2">
+          <span class="flex justify-betweeen mb-3">
+            <h3 class="w-full lg:w-10/12 text-xl font-medium">{{ form.question }}</h3>
+            <p class="w-full lg:w-2/12 text-right">
+              <button type="button" @click="toggleSubscribe(form._id)" :class="[matchForm(form._id) ? 'bg-blue-500 hover:bg-blue-600' : 'bg-green-500 hover:bg-green-600']"
+                class="inline-block focus:outline-none rounded text-white text-sm font-medium tracking-wide px-2 py-1">{{ matchForm(form._id) ? 'Unappend' : 'Append'}}</button>
+            </p>
+          </span>
+          <div v-if="form.type === 'text'">
+            <input type="text" class="appearance-none block w-full bg-white text-gray-700 border border-gray-500 rounded p-3 mb-1 leading-tight focus:outline-none">
+          </div>
+          <ul v-if="form.type !== 'input'">
+            <li v-if="form.type == 'checkbox'" v-for="option in form.form" class="flex mb-1">
+              <fa :icon="['fas', 'check-square']" class="text-gray-500 inline-block text-xl lg:text-2xl w-5 mr-2" />
+              <span>{{ option.value }}</span>
+            </li>
+            <li v-if="form.type == 'select'" class="relative z-0">
+              <select class="appearance-none block w-full bg-white text-gray-700 border border-gray-500 rounded p-3 mb-1 leading-tight focus:outline-none">
+                <option v-for="option in form.form">{{ option.value }}</option>
+              </select>
+              <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                </svg>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </li>
+    </ul>
+    <div class="text-right">
       <nuxt-link to="/admin" class="inline-block bg-gray-600 hover:bg-gray-700 focus:outline-none rounded text-white text-sm font-medium tracking-wide px-3 py-2 mr-1">
         Back
       </nuxt-link>
       <button class="bg-blue-500 hover:bg-blue-600 focus:outline-none rounded text-white text-sm font-medium tracking-wide px-3 py-2" type="submit">
         Save
       </button>
-    </p>
+    </div>
   </form>
 </div>
 </template>
@@ -55,6 +88,7 @@
 export default {
   data() {
     return {
+      forms: [],
       toDate: null,
       fromDate: null,
       showResponse: false,
@@ -64,7 +98,8 @@ export default {
         title: '',
         toDate: '',
         fromDate: '',
-        content: ''
+        content: '',
+        fid: []
       },
       errors: {
         title: false,
@@ -84,10 +119,21 @@ export default {
         this.object['fromDate'] = res.message.from
         this.object['content'] = res.message.content
         this.object['title'] = res.message.title
+        this.object['fid'] = res.message.fid || []
       }).catch(error => {
         console.log(error.response.data)
       })
     }
+
+    this.$axios.$get(process.env.API_URL + '/api/v1/challenge/task/form/list').then(res => {
+      this.forms = res.message
+    }).catch(error => {
+      if (error.hasOwnProperty('response')) {
+        console.log(error.response.data.message)
+      } else {
+        console.log(error.message)
+      }
+    })
   },
   props: {
     action: {
@@ -163,6 +209,17 @@ export default {
     notBeforeToday(date) {
       return date < this.yesterday
     },
+    toggleSubscribe(formId) {
+      if (!this.object.fid.includes(formId)) {
+        this.object.fid.push(formId)
+      } else {
+        const index = this.object.fid.findIndex(id => id === formId)
+        this.object.fid.splice(index, 1)
+      }
+    },
+    matchForm(formId) {
+      return this.object.fid.includes(formId)
+    },
     submitForm: function(e) {
       const isValidForm = (currentValue) => currentValue !== true
 
@@ -189,7 +246,8 @@ export default {
             'title': this.object.title.trim(),
             'content': this.object.content.trim(),
             'from_date': this.object.fromDate,
-            'to_date': this.object.toDate
+            'to_date': this.object.toDate,
+            'fid': this.object.fid
           }).then(res => {
             this.response = res.message
             this.responseError = false
@@ -209,7 +267,8 @@ export default {
             'title': this.object.title.trim(),
             'content': this.object.content.trim(),
             'from_date': this.object.fromDate,
-            'to_date': this.object.toDate
+            'to_date': this.object.toDate,
+            'fid': this.object.fid
           }).then(res => {
             this.response = res.data.message
             this.responseError = false

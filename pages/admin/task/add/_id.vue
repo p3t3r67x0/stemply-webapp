e771e0da939f51f2cc270954955405797506ee29
Aurelius Wendelken <webtobesocial@gmail.com>
2020-06-15 12:13:11 +0200

    changed header font size due to style guide rules

--- pages/admin/task/add/_id.vue
+++ pages/admin/task/add/_id.vue
@@ -2,7 +2,7 @@
 <div class="container mx-auto">
   <div class="mx-3 lg:mx-0">
     <div class="bg-white rounded-lg p-3">
-      <h1 class="text-2xl lg:text-4xl font-medium mb-2">Add task</h1>
+      <h1 class="text-xl lg:text-2xl font-medium mb-3">Add task</h1>
       <editor-component target="task" action="insert" :taskId="taskId" :challengeId="challengeId" />
     </div>
   </div>

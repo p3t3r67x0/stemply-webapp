0c52ff993e827375dea0a3bede8f5b4e4dba0b84
Aurelius Wendelken <webtobesocial@gmail.com>
2020-06-19 01:28:59 +0200

    added logic for handling checkbox questions in frontend

--- pages/account/task/_id.vue
+++ pages/account/task/_id.vue
@@ -3,25 +3,23 @@
   <div class="mx-3 lg:mx-0">
     <div class="bg-white rounded-lg p-3">
       <h1 class="text-xl lg:text-2xl font-bold">{{ task.title }}</h1>
-      <vue-markdown-plus class="markdown mb-6" :source="task.content" />
-      <form v-if="task.forms" @submit.prevent>
+      <vue-markdown-plus class="markdown" :source="task.content" />
+      <form v-if="task.forms" @submit.prevent class="mt-6">
+        <h2 class="text-2xl font-medium mb-3">Kontrollfragen</h2>
         <ul>
           <li v-for="form, index in task.forms">
-            <h2 class="text-xl font-semibold mb-3">Question</h2>
-            <div class="bg-gray-200 rounded-lg p-2">
-              <span class="flex justify-betweeen mb-3">
-                <h3 class="w-full lg:w-10/12 text-xl font-medium">{{ form.question }}</h3>
-              </span>
+            <div class="bg-gray-200 rounded-lg p-2 mb-3">
+              <h3 class="text-lg font-medium mb-3">{{ form.question }}</h3>
               <div v-if="form.type === 'text'">
-                <input v-model="task.forms[index].text" @focusout="submitForm(form._id, task._id, task.forms[index].text)" type="text"
+                <input v-model="task.forms[index].reply" @focusout="updateResponse(form._id, task._id, task.forms[index].reply)" type="text"
                   class="appearance-none block w-full bg-white text-gray-700 border border-gray-500 rounded p-3 mb-1 leading-tight focus:outline-none">
               </div>
               <div v-if="form.type == 'checkbox'" v-for="option, i in form.form" class="flex mb-1">
-                <fa @click="toggleCheckbox(form._id, task._id, i)" :class="[toggleCheckboxClass(form._id, task._id, i) ? 'text-green-500' : 'text-gray-500']" :icon="['fas', 'check-square']" class="inline-block text-xl lg:text-2xl w-5 mr-2" />
+                <fa :key="update" @click="toggleCheckbox(form._id, task._id, index, i)" :class="[setCheckboxClass(index, i) ? 'text-green-500' : 'text-gray-500']" :icon="['fas', 'check-square']" class="cursor-pointer inline-block text-xl lg:text-2xl w-5 mr-2" />
                 <span>{{ option.value }}</span>
               </div>
               <div v-if="form.type == 'select'" class="relative z-0">
-                <select v-model="task.forms[index].select" @change="submitForm(form._id, task._id, task.forms[index].select)"
+                <select v-model="task.forms[index].reply" @change="updateResponse(form._id, task._id, task.forms[index].reply)"
                   class="appearance-none block w-full bg-white text-gray-700 border border-gray-500 rounded p-3 mb-1 leading-tight focus:outline-none">
                   <option v-for="option in form.form">{{ option.value }}</option>
                 </select>
@@ -47,7 +45,7 @@ export default {
   data() {
     return {
       task: {},
-      checked: []
+      update: false
     }
   },
   created() {
@@ -73,56 +71,69 @@ export default {
   },
   middleware: 'auth',
   methods: {
-    toggleCheckboxClass(formId, taskId, index) {
+    setCheckboxClass(formIndex, checkboxIndex) {
+      const elementId = `checkbox${checkboxIndex}`
       let elementMatch = false
-      const elementId = `checkbox-${index}`
-      const object = {
-        formId: formId,
-        taskId: taskId,
-        elementId: elementId
+
+      if (!Array.isArray(this.task.forms[formIndex].reply)) {
+        this.task.forms[formIndex].reply = []
       }
 
-      this.checked.forEach(item => {
-        if (item.formId === object.formId && item.taskId === object.taskId && item.elementId === object.elementId) {
-          elementMatch = true
-        }
-      })
+      if (this.task.forms[formIndex].reply.includes(elementId)) {
+        elementMatch = true
+      }
 
       return elementMatch
     },
-    toggleCheckbox(formId, taskId, index) {
+    toggleCheckbox(formId, taskId, formIndex, checkboxIndex) {
+      const elementId = `checkbox${checkboxIndex}`
       let elementMatch = false
-      const elementId = `checkbox-${index}`
-      const object = {
-        formId: formId,
-        taskId: taskId,
-        elementId: elementId
-      }
 
-      this.checked.forEach(item => {
-        if (item.formId === object.formId && item.taskId === object.taskId && item.elementId === object.elementId) {
-          elementMatch = true
-        }
-      })
+      if (this.task.forms[formIndex].reply.includes(elementId)) {
+        elementMatch = true
+      }
 
       if (elementMatch) {
-        const elementMatchId = this.checked.findIndex(e => e.elementId === elementMatch.elementId)
-        this.checked.splice(elementMatchId, 1)
+        const elementIndex = this.task.forms[formIndex].reply.findIndex(e => e === elementId)
+        this.task.forms[formIndex].reply.splice(elementIndex, 1)
+        this.removeResponse(formId, taskId, elementId)
       } else {
-        this.checked.push(object)
+        this.task.forms[formIndex].reply.push(elementId)
+        this.updateResponse(formId, taskId, this.task.forms[formIndex].reply)
       }
 
-      this.submitForm(formId, taskId, this.checked.map(e => e['elementId']))
+      this.update = !this.update
     },
-    submitForm(formId, taskId, value) {
+    updateResponse(formId, taskId, value) {
+      if (Array.isArray(value)) {
+        value = value.filter(e => e)
+      }
+
       this.$axios.$post(`${process.env.API_URL}/api/v1/challenge/task/response`, {
         fid: formId,
         tid: taskId,
         reply: value
       }).then(res => {
         console.log(res)
-        this.response = res.message
-        this.showResponse = true
+      }).catch(error => {
+        if (error.hasOwnProperty('response')) {
+          console.log(error.response.data)
+        } else {
+          console.log(error.message)
+        }
+      })
+    },
+    removeResponse(formId, taskId, value) {
+      if (Array.isArray(value)) {
+        value = value.filter(e => e)
+      }
+
+      this.$axios.$put(`${process.env.API_URL}/api/v1/challenge/task/response`, {
+        fid: formId,
+        tid: taskId,
+        reply: value
+      }).then(res => {
+        console.log(res)
       }).catch(error => {
         if (error.hasOwnProperty('response')) {
           console.log(error.response.data)
